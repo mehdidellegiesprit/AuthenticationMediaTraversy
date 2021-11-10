17551b7baf510b77b74d10f80f7025d5c5af9fdd
mehdi <mehdi.dellegi@esprit.tn>
2021-11-10 15:05:02 -0600

    second commit

---
+++ routes/api/auth.js
@@ -0,0 +1,9 @@
+const { decodeBase64 } = require("bcryptjs");
+const express = require("express");
+const router = express.Router();
+
+// @route  GET api/auth
+// @desc   Test route
+// @access Public
+router.get("/", (req, res) => res.send("Auth route"));
+module.exports = router;
---
+++ routes/api/profile.js
@@ -0,0 +1,9 @@
+const { decodeBase64 } = require("bcryptjs");
+const express = require("express");
+const router = express.Router();
+
+// @route  GET api/profile
+// @desc   Test route
+// @access Public
+router.get("/", (req, res) => res.send("Profile route"));
+module.exports = router;
