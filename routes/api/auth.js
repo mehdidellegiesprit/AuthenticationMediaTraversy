711b94b453f0438dd912449f482984a661a33d1e
mehdi <mehdi.dellegi@esprit.tn>
2021-12-01 08:37:07 -0600

    after delete post

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,81 @@
 const { decodeBase64 } = require("bcryptjs");
 const express = require("express");
 const router = express.Router();
-
+const bcrypt = require("bcryptjs");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator/check");
 // @route  GET api/auth
 // @desc   Test route
 // @access Public
-router.get("/", (req, res) => res.send("Auth route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    return res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send("Server Error");
+  }
+});
+// @route  POST api/auth
+// @desc   Authenticate user & get token
+// @access Public
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password", 
+      "Password is required "
+    ).exists(),
+  ],
+  async (req, res) => {
+    // console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {  email, password } = req.body;
+
+    try {
+      //See if user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // we must verify if the password match exact 
+      const isMatch = await bcrypt.compare(password,user.password)
+      if (!isMatch){
+        return res
+            .status(400)
+            .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+      //Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          return res.json({ token });
+        }
+      );
+
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 module.exports = router;
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,97 @@
 const { decodeBase64 } = require("bcryptjs");
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+const auth = require("../../middleware/auth");
 
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route  POST api/posts
+// @desc   Create a post
+// @access Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+      const post = await newPost.save();
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 // @route  GET api/posts
-// @desc   Test route
-// @access Public
-router.get("/", (req, res) => res.send("Post route"));
+// @desc   Get all posts
+// @access Private
+router.get("/", [auth], async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json({ "Nbre postes : ": posts.length, posts });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+// @route  GET api/posts/:id
+// @desc   Get =post by ID
+// @access Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "post not found" });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res
+        .status(404)
+        .json({ msg: "post not found => object id not valid" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+// @route  DELETE api/posts/:id
+// @desc   Delete a post by ID
+// @access Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "post not found" });
+    }
+    // make sure that the user that will delete the post is the owner of the post
+    // check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+    await post.remove();
+    res.json({ msg: "Post removed " });
+
+} catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res
+        .status(404)
+        .json({ msg: "post not found => object id not valid" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
